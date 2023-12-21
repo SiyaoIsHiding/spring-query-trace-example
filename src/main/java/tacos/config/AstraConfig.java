15eb2efa525d9b47c90fd105bceb9690b56c14f8
janehe <siyaoh4@uci.edu>
2023-12-20 19:26:45 -0800

    tested connection

--- src/main/java/tacos/config/AstraConfig.java
+++ src/main/java/tacos/config/AstraConfig.java
@@ -3,6 +3,7 @@ package tacos.config;
 import java.io.File;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -10,6 +11,7 @@ import lombok.Setter;
 @ConfigurationProperties(prefix = "datastax.astra")
 @Getter
 @Setter
+@Component
 public class AstraConfig {
     private File secureConnectBundle;
 }
--- src/main/java/tacos/config/CassandraConfig.java
+++ src/main/java/tacos/config/CassandraConfig.java
@@ -16,9 +16,6 @@ public class CassandraConfig  {
     @Value("${spring.data.cassandra.keyspace-name}")
     private String keyspace;
 
-    @Value("${spring.data.cassandra.local-datacenter}")
-    private String localDatacenter;
-
     @Value("${spring.data.cassandra.username}")
     private String username;
 
@@ -29,7 +26,6 @@ public class CassandraConfig  {
     @Bean
     CqlSession cassandraSession(QueryTraceCache traceCache, AstraConfig astraProperties) {
         CqlSession delegate = CqlSession.builder().withKeyspace(keyspace)
-        .withLocalDatacenter(localDatacenter)
         .withAuthCredentials(username, password)
         .withCloudSecureConnectBundle(astraProperties.getSecureConnectBundle().toPath()).build();
         return new QueryTraceCqlSession(delegate, traceCache);
