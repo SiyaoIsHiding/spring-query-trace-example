733f0803de24102bcd453c93947c294be6d28877
janehe <siyaoh4@uci.edu>
2024-01-05 10:01:53 -0800

    change to guava cache

--- src/main/java/tacos/data/QueryTraceCache.java
+++ src/main/java/tacos/data/QueryTraceCache.java
@@ -1,12 +1,14 @@
 package tacos.data;
 
+import java.util.UUID;
+
 import org.springframework.stereotype.Component;
 
-import com.datastax.oss.driver.api.core.cql.TraceEvent;
-import com.datastax.oss.driver.shaded.guava.common.collect.EvictingQueue;
+import com.datastax.oss.driver.api.core.cql.QueryTrace;
+import com.datastax.oss.driver.shaded.guava.common.cache.Cache;
+import com.datastax.oss.driver.shaded.guava.common.cache.CacheBuilder;
 
 @Component
 public class QueryTraceCache {
-    public EvictingQueue<TraceEvent> cache = EvictingQueue.create(100);
-    
+    public Cache<UUID, QueryTrace> cache = CacheBuilder.newBuilder().maximumSize(100).build();
 }
